614b7f1fea43e4d1f511ad27846bdd5b5656bb01
Kiwi-Eric-Li <lxf7756@126.com>
2026-03-20 17:35:23 +0800

    开发登录页面

--- components/Login.js
+++ components/Login.js
@@ -1,4 +1,4 @@
-import {View, Text, TextInput, StyleSheet} from 'react-native'
+import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native'
 
 
 const Input = (props) => {
@@ -27,6 +27,41 @@ const Input = (props) => {
     )
 }
 
+export const ConfirmButton = (props) => {
+    const {title, onClick} = props;
+    return (
+        <TouchableOpacity style={styles.confirmLayout} onPress={onClick}>
+            <Text style={styles.confirmTitle}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export const Tips = (props) => {
+    const {msg} = props;
+    return (
+        <View style={styles.tipsLayout}>
+            <Text style={styles.tips}>{msg}</Text>
+        </View>
+    )
+}
+
+export const NavBar = (props) => {
+    const {title, rightTitle, onRightClick} = props;
+
+    return (
+        <View style={styles.navBar}>
+            <View />
+            <View style={styles.titleLayout}>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+            <Text style={styles.rightTitle}>{rightTitle}</Text>
+        </View>
+    )
+
+
+}
+
+
 const styles = StyleSheet.create({
     row: {
         flexDirection: 'row'
@@ -41,6 +76,50 @@ const styles = StyleSheet.create({
     input: {
         flex: 1,
         marginRight: 15
+    },
+    confirmLayout: {
+        backgroundColor: '#2196f3',
+        alignItems: 'center',
+        padding: 12,
+        margin: 20,
+        marginTop: 30,
+        borderRadius: 5
+    },
+    confirmTitle: {
+        color: '#fff',
+        fontSize: 20
+    },
+    tipsLayout: {
+        alignItems: 'center',
+        marginTop: 10
+    },
+    tips: {
+        fontSize: 14,
+        color: 'red'
+    },
+    navBar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        height: 44,
+        marginLeft: 10,
+        marginRight: 10
+    },
+    titleLayout: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        left: 40,
+        right: 40,
+        top: 0,
+        bottom: 0
+    },
+    title: {
+        fontSize: 20,
+        color: 'black'
+    },
+    rightTitle: {
+        color: '#2196f3'
     }
 })
 
--- page/LoginPage.js
+++ page/LoginPage.js
@@ -1,15 +1,26 @@
 import {useState} from 'react'
 import {View, StyleSheet} from 'react-native'
 
-import Input from '../components/Login'
+import Input, { ConfirmButton, Tips, NavBar } from '../components/Login'
 
 export default () => {
 
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
+    const [msg, setMsg] = useState("");
+
+    const onLogin = () => {
+        if(userName === '' || password === ''){
+            setMsg('The username or password cannot be empty.');
+            return ;
+        }
+        setMsg('');
+    }
 
     return (
         <View style={styles.root}>
+            <NavBar title="Login" rightTitle="Register"/>
+            <View style={styles.line}/>
             <View style={styles.content}>
                 <Input 
                     label="Username" 
@@ -23,6 +34,8 @@ export default () => {
                     secure={true}
                     onChangeText={(txt) => setPassword(txt)} 
                 />
+                <ConfirmButton title="Login" onClick={onLogin}/>
+                <Tips msg={msg}/>
             </View>
         </View>
     )
@@ -30,11 +43,16 @@ export default () => {
 
 const styles = StyleSheet.create({
     root: {
-        flex: 1
+        flex: 1,
+        marginTop: 30
     },
     content: {
         paddingTop: 20,
         backgroundColor: '#F1F5F6',
         flexGrow: 1
+    },
+    line: {
+        height: 0.5,
+        backgroundColor: '#D0D4D4'
     }
 })
